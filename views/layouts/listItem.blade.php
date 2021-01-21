fa15706901c00f77a6d94b0512f3940678b5599d
weratad20 <weratad20@gmail.com>
2021-01-21 13:14:24 +0700

    fixd username

--- views/layouts/listItem.blade.php
+++ views/layouts/listItem.blade.php
@@ -33,7 +33,7 @@
         {{-- center side --}}
         <td>
         <p data-id="{{ $type.'_'.$user->id }}">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }} 
+            {{ strlen($user->username) > 12 ? trim(substr($user->username,0,12)).'..' : $user->username }}
             <span>{{ $lastMessage->created_at->diffForHumans() }}</span></p>
         <span>
             {{-- Last Message user indicator --}}
@@ -74,7 +74,7 @@
         {{-- center side --}}
         <td>
         <p data-id="{{ $type.'_'.$user->id }}">
-            {{ strlen($user->name) > 12 ? trim(substr($user->name,0,12)).'..' : $user->name }} 
+            {{ strlen($user->username) > 12 ? trim(substr(($user->username,0,12)).'..' : ($user->username }}
         </td>
         
     </tr>
